caced26ca229dc90b3d19e0b3c7f746dce0e2ea0
ETi0n <148210094+ETi0n@users.noreply.github.com>
2024-08-04 19:21:54 +0900

    Title: 정수 선언하고 뺄셈, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 240804/정수 선언하고 뺄셈/define-numbers-and-substract.java
+++ 240804/정수 선언하고 뺄셈/define-numbers-and-substract.java
@@ -2,8 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int a = 97;
         int b = 13;
-    System.out.print(a + " - "); 
-    System.out.print(b +  " = ");
+        System.out.print(a + " - "); 
+        System.out.print(b +  " = ");
         System.out.print(a - b);
     }
 }
